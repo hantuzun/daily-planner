aa4aedb101d9941fe2a7e39766a07509405ea4e3
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-05-28 18:53:51 +0300

    Add cover page

--- daily_planner.py
+++ daily_planner.py
@@ -11,32 +11,51 @@ output_file = 'output.pdf'
 shutil.rmtree(files_dir, ignore_errors=True)
 os.makedirs(files_dir)
 
-# Set dates
+# Set parameters
+author = 'Emrehan T\\"uz\\"un'
 first_day = date(2015, 6, 1)
 last_day = date(2015, 8, 31)
-dates = [first_day + timedelta(days=x) for x in range((last_day-first_day).days + 1)]
 
-# Create files with date strings
+
+# Create a data file for the cover page
+cover_data = os.path.join(files_dir, 'cover')
+with open(cover_data, 'wb') as f:
+    f.write(bytes('\\def\\author{' + author + '}\n', 'UTF-8'))
+    f.write(bytes('\\def\\firstDay{' + first_day.strftime('%-d %B %Y') + '}\n', 'UTF-8'))
+    f.write(bytes('\\def\\lastDay{' + last_day.strftime('%-d %B %Y') + '}\n', 'UTF-8'))
+
+# Create the cover page pdf
+print('Creating cover page')
+subprocess.Popen(shlex.split('pdflatex -interaction=batchmode -jobname=' + cover_data + ' cover.tex')).communicate()
+print()
+
+# Create data files for date pages
+dates = [first_day + timedelta(days=x) for x in range((last_day-first_day).days + 1)]
 for d in dates:
-    filename = str(d)
-    directory = os.path.join(files_dir, filename)
+    f = str(d) + '.txt'
+    directory = os.path.join(files_dir, f)
     with open(directory, 'wb') as f:
-        f.write(bytes('\def\date{' + d.strftime("%-d %B") + '}\n', 'UTF-8'))
-        f.write(bytes('\def\dayOfWeek{' + d.strftime("%A") + '}\n', 'UTF-8'))
+        f.write(bytes('\\def\\date{' + d.strftime('%-d %B') + '}\n', 'UTF-8'))
+        f.write(bytes('\\def\\dayOfWeek{' + d.strftime('%A') + '}\n', 'UTF-8'))
 
 # Create pdf files using daily-planner.tex and date files
-for filename in os.listdir(files_dir):
-    directory = os.path.join(files_dir, filename)
-    print('Creating ' + filename + ' page')
+txt_files = [f for f in os.listdir(files_dir) if f.endswith('txt')]
+for f in txt_files:
+    directory = os.path.join(files_dir, f)
+    print('Creating ' + f + ' page')
     subprocess.Popen(shlex.split('pdflatex -interaction=batchmode -jobname=' + directory + ' daily-planner.tex')).communicate()
     print()
 
 # Merge pdf files into one
-pdf_files = [f for f in os.listdir(files_dir) if f.endswith("pdf")]
 merger = PdfFileMerger()
 
-for filename in pdf_files:
-    merger.append(PdfFileReader(os.path.join(files_dir, filename), "rb"))
+cover_pdf = os.path.join(files_dir, 'cover.pdf')
+merger.append(PdfFileReader(cover_pdf, 'rb'))
+os.remove(cover_pdf)
+
+pdf_files = [f for f in os.listdir(files_dir) if f.endswith('pdf')]
+for f in pdf_files:
+    merger.append(PdfFileReader(os.path.join(files_dir, f), 'rb'))
 
 merger.write(output_file)
 print(output_file + ' is created')
